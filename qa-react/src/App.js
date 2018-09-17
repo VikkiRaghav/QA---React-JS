9f9861f65763f1cbcfddbe9557ce1b8d90497787
unknown <vishalraghav101993@gmail.com>
2018-09-16 22:22:28 -0400

    Second Commit

--- qa-react/src/App.js
+++ qa-react/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-
+import NavBar from './NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div>
+      <NavBar/>
        <p>Work in Progress </p>
        </div>
     );
